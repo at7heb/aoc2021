e605fec777b8515ab554a7cac7a86fbc46a84b1f
Howard Bussey <howard.bussey@gmail.com>
2021-12-13 00:05:43 -0500

    checkpoint

--- apps/day9/lib/day9.ex
+++ apps/day9/lib/day9.ex
@@ -3,16 +3,77 @@ defmodule Day9 do
   Documentation for `Day9`.
   """
 
-  @doc """
-  Hello world.
+  def run() do
+    File.read!("input.txt")
+    |> process()
+  end
 
-  ## Examples
+  def example() do
+    input = """
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
+"""
+    process(input)
+  end
 
-      iex> Day9.hello()
-      :world
+  def process(input) do
+    String.trim(input)
+    |> String.split("\n", trim: true)    #list of lines
+    |> make_model()
+    |> analyze_model()
+  end
 
-  """
-  def hello do
-    :world
+  def analyze_model(model) do
+    # model is a tuple of tuples
+    %{model: model, low_points: %{}}
+    |> analyze_corners()
+    |> analyze_edges()
+    |> analyze_interior()
+  end
+
+  def analyze_corners(puzzle) do
+    puzzle
+    |> analyze_a_corner(:top, :left)
+    |> analyze_a_corner(:top, :right)
+    |> analyze_a_corner(:bottom, :left)
+    |> analyze_a_corner(:bottom, :right)
+  end
+
+  def analyze_a_corner(puzzle, vert, horiz) do
+    puzzle
+  end
+
+  def analyze_edges(puzzle) do
+    puzzle
+    |> analyze_an_edge(:top)
+    |> analyze_an_edge(:rigght)
+    |> analyze_an_edge(:bottom)
+    |> analyze_an_edge(:left)
+  end
+
+  def analyze_an_edge(puzzle, position) do
+    puzzle
+  end
+
+  def analyze_interior(puzzle) do
+    puzzle
+  end
+
+  def model_value(%{model: model} = _puzzle, row, col) do
+    elem(model, row)
+    |> elem(col)
+  end
+
+  def make_model(list_of_lines) do
+    Enum.reduce(list_of_lines, {}, fn line, tupl -> Tuple.append(tupl, model_line(line)) end)
+  end
+
+  def model_line(line) do
+    String.split(line, "", trim: true)
+    |> Enum.map(fn d -> String.to_integer(d) end)
+    |> Enum.reduce({}, fn val, tupl -> Tuple.append(tupl, val) end)
   end
 end
