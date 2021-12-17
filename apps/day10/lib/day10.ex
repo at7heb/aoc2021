8246e9a43a7cd321c7e458a74eac92bd34098270
Howard Bussey <howard.bussey@gmail.com>
2021-12-16 23:26:46 -0500

    day10 part 1 complete

--- apps/day10/lib/day10.ex
+++ apps/day10/lib/day10.ex
@@ -3,16 +3,89 @@ defmodule Day10 do
   Documentation for `Day10`.
   """
 
-  @doc """
-  Hello world.
+  def run() do
+    File.read!("input.txt")
+    |> process()
+    |> calculate_scores()
+    |> present()
+  end
 
-  ## Examples
+  def example() do
+    process("""
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+""")
+    |> calculate_scores()
+    |> present()
+  end
 
-      iex> Day10.hello()
-      :world
+  def process(s) do
+    s
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> analyze_line(line) end)
+  end
 
-  """
-  def hello do
-    :world
+  def analyze_line(line) do
+    String.codepoints(line)
+    |> analyze_line([], line)
+  end
+
+  def analyze_line([], [], line), do: {:ok, line} |> IO.inspect()
+
+  def analyze_line([], _, line), do: {:incomplete, line} |> IO.inspect
+
+  def analyze_line([first| rest] = _code_points, state, line) when is_list(state) do
+    cond do
+      String.contains?("([{<", first) -> analyze_line(rest, [first | state], line)
+      String.contains?(")]}>", first) -> analyze_or_error_stop(first, rest, state, line)
+      true                           -> {:illegal, "unexpected character #{first}"} |> IO.inspect()
+    end
+  end
+
+  def analyze_or_error_stop(_first, _rest, [], line), do: {:incomplete, line}
+
+  def analyze_or_error_stop(first, rest, [state_first|state_rest] = _state, line) do
+    case first <> state_first do
+      ")(" -> analyze_line(rest, state_rest, line)
+      "><" -> analyze_line(rest, state_rest, line)
+      "][" -> analyze_line(rest, state_rest, line)
+      "}{" -> analyze_line(rest, state_rest, line)
+      _    -> {:error, first, line} |> IO.inspect()
+    end
+
+  end
+
+  def calculate_scores(line_results) do
+    #line results are {:ok, "(())"}
+    # or {:error, ">", "(()>"}
+    # and maybe {:incomplete, "(()"}
+    line_results
+    |> Enum.filter(fn tuple -> elem(tuple, 0) == :error end) |> IO.inspect()
+    |> Enum.map(fn tuple -> point_value(elem(tuple, 1)) end) |> IO.inspect()
+    |> Enum.sum()
+  end
+
+  def point_value(str) do
+    case str do
+      ")" ->     3
+      "]" ->    57
+      "}" ->  1197
+      ">" -> 25137
+      _   -> -9_999_999_999
+    end
+  end
+
+  def present(value) do
+    IO.puts("The syntax error score is #{value}")
+    value
   end
 end
--- apps/day10/test/day10_test.exs
+++ apps/day10/test/day10_test.exs
@@ -2,7 +2,25 @@ defmodule Day10Test do
   use ExUnit.Case
   doctest Day10
 
-  test "greets the world" do
-    assert Day10.hello() == :world
+  test "null string" do
+    assert Day10.process("") == []
+  end
+
+  test "balanced strings" do
+    assert hd(Day10.process("()")) == {:ok, "()"}
+    assert hd(Day10.process("[]")) == {:ok, "[]"}
+    assert hd(Day10.process("{}")) == {:ok, "{}"}
+    assert hd(Day10.process("<>")) == {:ok, "<>"}
+  end
+
+  test "incomplete" do
+    assert hd(Day10.process("(")) == {:incomplete, "("}
+    assert hd(Day10.process("[")) == {:incomplete, "["}
+    assert hd(Day10.process("{")) == {:incomplete, "{"}
+    assert hd(Day10.process("<")) == {:incomplete, "<"}
+  end
+
+  test "error" do
+    assert {:error, ")", "{)"} == hd(Day10.process("{)"))
   end
 end
